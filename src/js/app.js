76ea701367ec9a5d4788da5c6a056421a07dc09c
TomasS <43637908+GrimReaper6667@users.noreply.github.com>
2020-02-11 00:38:37 +0100

    test

--- src/js/app.js
+++ src/js/app.js
@@ -35,6 +35,20 @@ function init(){
     //     name: "Horror"
     // })
 
+    const searchBtn = document.querySelector('a[href="#search"]');
+    const searchValue = document.querySelector("input[type=text]")
+
+   
+
+    searchValue.addEventListener("keydown", (e)=>{
+        if(e.keyCode == 13){
+            e.preventDefault();
+            searchBtn.href += `/${searchValue.value}`;
+            window.location = searchBtn.href;
+            // window.open(searchBtn.href)
+        }
+    })
+
     routie({
         'movie/:id': function(id) {
 
